67dce4f8ae9e2b57245e1d930a8375b458ade186
Alex Flint <alex.flint@gmail.com>
2017-02-07 17:35:48 -0800

    add printjson

---
+++ printjson.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+import "io/ioutil"
+
+func fail(msg interface{}, parts ...interface{}) {
+	fmt.Printf(fmt.Sprintf("%v", msg), parts...)
+	os.Exit(1)
+}
+func main() {
+	if len(os.Args) > 2 {
+		fail("usage: printjson [FILE]")
+	}
+
+	var err error
+	var buf []byte
+	if len(os.Args) < 2 || os.Args[1] == "-" {
+		buf, err = ioutil.ReadAll(os.Stdin)
+	} else {
+		buf, err = ioutil.ReadFile(os.Args[1])
+	}
+	if err != nil {
+		fail(err)
+	}
+
+	var b bytes.Buffer
+	err = json.Indent(&b, buf, "", "  ")
+	if err != nil {
+		fail(err)
+	}
+	b.WriteTo(os.Stdout)
+}
